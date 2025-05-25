4eefed4133deb9bb9eade7422b32578ad59ae465
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-25 23:59:06 +0530

    a complex project - blog site

---
+++ src/appwrite/auth.js
@@ -0,0 +1,18 @@
+import conf from '../conf.js';
+import { Client, Account, ID } from 'appwrite';
+
+export class AuthService{
+    client = new Client()
+    account;
+
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
