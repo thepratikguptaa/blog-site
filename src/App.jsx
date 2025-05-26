725ebb4e2fb502c10f8d7b4f5799a794accc7cde
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-26 15:53:05 +0530

    a complex project - blog site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth' // Importing the authService to check user authentication
+import { login, logout} from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog Site</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser().then((userData) => {
+      if (userData) {
+        dispatch(login({ userData }))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }, [dispatch])
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null // Return null while loading to avoid rendering the app before user data is fetched
 }
 
 export default App
