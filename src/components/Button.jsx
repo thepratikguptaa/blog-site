a35a8f43a8f5ba42bbf8e2eed81ba48c90e591e9
thepratikguptaa <pratikguptamain@gmail.com>
2025-05-27 23:12:23 +0530

    blog

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 // Button component for reuse
 function Button({
-    children, type = 'button',bgColor = 'bg-teal-600',
+    children, type = 'button',bgColor = 'bg-gradient-to-r from-teal-500 to-emerald-500 hover:from-teal-600 hover:to-emerald-600',
     textColor = 'text-white',
     className = '',
     ...props
 }) {
   return (
-    <button className={`w-full bg-gradient-to-r from-teal-500 to-emerald-500 hover:from-teal-600 hover:to-emerald-600 transform hover:scale-[1.02] transition-all duration-300 py-3 text-lg font-semibold text-white rounded-lg shadow-md ${bgColor} ${textColor} ${className}`}type={type} {...props}>
+    <button className={`w-full  transform hover:scale-[1.02] transition-all duration-300 py-3 text-lg font-semibold text-white rounded-lg shadow-md ${bgColor} ${textColor} ${className}`}type={type} {...props}>
         {children}
     </button>
   )
